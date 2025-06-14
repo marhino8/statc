38bc425d2364222100296facbf98cee7aa351d3b
marhino <marhino@email.com>
2025-06-15 00:25:37 +0300

    add logs

--- src/main/java/com/amdprofile/statcollector/Controller.java
+++ src/main/java/com/amdprofile/statcollector/Controller.java
@@ -1,11 +1,13 @@
 package com.amdprofile.statcollector;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/record")
@@ -16,12 +18,14 @@ public class Controller {
 
     @PostMapping("/save")
     public ResponseEntity<Void> saveRecord(@RequestBody Record record) {
+        log.info("save endpoint called with request body: {}", record);
         recordService.saveRecord(record);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/getAll")
     public ResponseEntity<List<Record>> getAllRecords() {
+        log.info("getAll endpoint called");
         return ResponseEntity.ok().body(recordService.getAllRecords());
     }
 
--- src/main/java/com/amdprofile/statcollector/Record.java
+++ src/main/java/com/amdprofile/statcollector/Record.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Setter
 @Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
